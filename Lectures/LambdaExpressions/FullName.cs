0ab94909468c0afe3434d9d5633505f1761389d5
Vera <vkorchemnaya@eagles.ewu.edu>
2023-02-16 15:20:59 -0800

    lskjfd

---
+++ Lectures/LambdaExpressions/FullName.cs
@@ -0,0 +1,36 @@
+﻿namespace LambdaExpressions;
+public record FullName(string FirstName, string LastName)
+{
+    public FullName(string fullName) : this(null!, null!)
+    {
+        // Pattern matching by 
+        // DO not use regular expressions in the homework
+        if (fullName.Split(" ") is [string firstName, string lastName])
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //if(fullName.Split(" ") is ["Inigo", string lastName])
+        //{
+        //    FirstName = "Inigo";
+        //    LastName = lastName;
+        //}
+    }
+
+    // Use pattern matching to return the initials of the FullName
+    // Implement a Name property or override the ToString() to return the full name
+    // Hints:
+    //      Using multiple list patterns
+    //      User recursion pattern matching
+
+
+    public string Initials
+    {
+        get
+        {
+            return null;
+        }
+    }
+
+}
---
+++ Lectures/LambdaExpressions/PatternMatchingTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LambdaExpressions;
+
+[TestClass]
+public class PatternMatchingTests
+{
+    [TestMethod]
+    public void SetName_InigoMontoya_Success()
+    {
+        FullName testName = new FullName("Inigo Montoya");
+        (string, string) actual = (testName.FirstName, testName.LastName);
+        Assert.AreEqual<(string, string)>(("Inigo", "Montoya"), actual);
+        Assert.IsTrue(testName.FirstName is "Inigo");
+        // pattern matching to a constrant
+        // when pattern matching we car use is or a switch with is
+
+    }
+
+
+    [TestMethod]
+    public void UsingSwitch_InitialsInigoMontoya_Success()
+    {
+        FullName testName = new FullName("Inigo Montoya");
+
+        string actual = testName.Initials;
+        Assert.AreEqual<string>("IM", actual);
+
+    }
+}
